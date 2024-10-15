2b9c3a054bc110f52c2e83e0cd64b2d571e43a46
Kamranalisuhag <kamranalisuhag9@gmail.com>
2024-10-15 04:26:55 -0700

    SMS website

---
+++ about-us.php
@@ -0,0 +1,50 @@
+<?php include('shared/_header.php');?>
+
+<body>
+    <main>
+        <div class="big-wrapper light">
+            <img src="./images/shape.png" alt="" class="shape" />
+
+            
+     <?php include('shared/_navbar.php'); ?>
+
+
+
+            <div class="container mt-5">
+
+
+    
+                <div class="card border-0 shadow about-card">
+                    <div class="card-body mt-5 mb-2 mx-3 ">
+                        <h3 class="card-title">
+                        <span class="text-primary icon-hover"><i class="fa-solid fa-paperclip"></i></span>    
+                        About us</h3>
+                        <p class="card-text fs-5 text-justify">
+                        The goals of a school management system go beyond simple administrative comfort in the dynamic field of education. These systems promote efficiency, transparency, and cooperation inside educational institutions by acting as catalysts for positive chang
+                    </div>
+                    <div class="mb-4" style="width: 100%;  display: flex; align-items: center; justify-content: center;">
+                    <img style="width: 400px;"  src="./images/company-logo.png" class="card-img-bottom" alt="...">
+                    </div>
+                </div>
+
+
+                
+            </div>
+
+
+
+
+
+
+
+
+        </div>
+
+
+    </main>
+
+
+
+
+  
+    <?php include('shared/_footer.php'); ?>
